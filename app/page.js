8655872d4b318fbacddf87da200acee35c851ec4
advika31 <advikasinghal31@gmail.com>
2025-06-11 12:21:15 +0530

    setting up the project

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Advika</div>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
