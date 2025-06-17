3571d74376900a138ee2a513a4d883bc3e0b0476
advika31 <advikasinghal31@gmail.com>
2025-06-17 22:09:49 +0530

    user auth

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import { ThemeProvider } from "@/components/theme-provider";
+import {ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes';
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,27 +14,29 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
+    <ClerkProvider appearance={{baseTheme: dark}}>
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className}`}> 
-        {/* header */}
+      <body className={`${inter.className}`}>
         <ThemeProvider
-            attribute="class"
-            defaultTheme="dark"
-            enableSystem
-            disableTransitionOnChange
-          >
-            
-        <main className="min-h-screen">{children}</main>
-        
-        {/* footer */}
-        <footer className="bg-muted/50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-200">
-            <p> Advika </p>
-          </div>
-        </footer>
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {/* header */}
+          <Header />
 
-          </ThemeProvider>
+          <main className="min-h-screen">{children}</main>
+
+          {/* footer */}
+          <footer className="bg-muted/50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-200">
+              <p> Advika </p>
+            </div>
+          </footer>
+        </ThemeProvider>
       </body>
     </html>
+    </ClerkProvider>
   );
 }
