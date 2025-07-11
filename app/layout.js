5042088436b9b1c0723362c58210e6f1c4951695
advika31 <advikasinghal31@gmail.com>
2025-07-11 23:40:54 +0530

    update layout.js

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ import { ThemeProvider } from "@/components/theme-provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Doctors Appointment App",
+  title: "DocVia",
   description: "Connect with doctors anytime, anywhere",
 };
 
@@ -35,11 +35,6 @@ export default function RootLayout({ children }) {
             <main className="min-h-screen">{children}</main>
             <Toaster richColors />
 
-            <footer className="bg-muted/50 py-12">
-              <div className="container mx-auto px-4 text-center text-gray-200">
-                <p>Made with 💗 by RoadsideCoder</p>
-              </div>
-            </footer>
           </ThemeProvider>
         </body>
       </html>
